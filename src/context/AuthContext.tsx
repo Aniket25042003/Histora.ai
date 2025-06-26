2cf3cb6e1d690bca12c8377a7538dcd6ff5e04ed
Aniket25042003 <aniketg2003patel@gmail.com>
2025-06-26 13:24:32 -0400

    Sign in issue fix

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -52,7 +52,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `http://localhost:5173/dashboard`
+          redirectTo: `https://tpgzxparxxyxbmpsfwkt.supabase.co/auth/v1/callback`
         }
       })
       if (error) throw error
